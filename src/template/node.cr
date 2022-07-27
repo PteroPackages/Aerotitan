0912cf52ef5d763aec2d35c823fe5db5a5553e6b
devnote-dev <inferno0745@gmail.com>
2022-07-27 16:12:35 +0100

    feat(template): accepts for nodes

--- src/template/node.cr
+++ src/template/node.cr
@@ -8,6 +8,10 @@ abstract struct Node
     @start = token.start
     @end = token.end
   end
+
+  def accepts?(other : Node) : Bool
+    false
+  end
 end
 
 struct Nop < Node
@@ -38,6 +42,14 @@ struct NumberLiteral < Node
     super token
     @value = token.value.not_nil!.to_f
   end
+
+  def accepts?(other : Node) : Bool
+    other.is_a? self.class
+  end
+
+  def to_s : String
+    "number"
+  end
 end
 
 struct StringLiteral < Node
@@ -46,6 +58,14 @@ struct StringLiteral < Node
   def initialize(token)
     super token
     @value = token.value.not_nil!
+  end
+
+  def accepts?(other : Node) : Bool
+    other.is_a? self.class || other.is_a? String
+  end
+
+  def to_s : String
+    "string"
   end
 end
 
@@ -62,11 +82,27 @@ struct BoolLiteral < Node
       raise "invalid boolean literal"
     end
   end
+
+  def accepts?(other : Node) : Bool
+    other.is_a? self.class || other.is_a? Bool
+  end
+
+  def to_s : String
+    "boolean"
+  end
 end
 
 struct NullLiteral < Node
   def initialize(token)
     super token
+  end
+
+  def accepts?(other : Node) : Bool
+    other.is_a? self.class || other.nil?
+  end
+
+  def to_s : String
+    "null"
   end
 end
 
