da7b37f74cde513adfa0b621be438b0e25996d58
devnote-dev <inferno0745@gmail.com>
2022-09-17 00:19:21 +0100

    feat: base for commands

--- src/aerotitan.cr
+++ src/aerotitan.cr
@@ -1,7 +1,19 @@
+require "cli"
+
 require "./bindings"
 require "./compile"
 require "./syntax/*"
 
 module Aerotitan
   VERSION = "0.1.0"
+
+  def self.run : Nil
+    app = CLI::Application.new
+    app.help_template = Commands::RootCommand.help_template
+    app.add_command Commands::RootCommand, default: true
+
+    app.run ARGV
+  end
 end
+
+Aerotitan.run
