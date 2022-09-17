8f3cb0cccdd95444113b9814ce527162be2cbb04
devnote-dev <inferno0745@gmail.com>
2022-09-17 01:46:50 +0100

    feat(actions): more app actions

--- src/actions.cr
+++ src/actions.cr
@@ -1,11 +1,13 @@
 module Aerotitan::Actions
   # key => {can_use_query, requires_data}
   APP_ACTIONS = {
+    "users:select" => {true, false},
     # "users:create" => {false, true},
-    "users:update" => {true, true},
+    # "users:update" => {true, true},
     "users:delete" => {true, false},
+    "servers:select" => {true, false},
     # "servers:create" => {true, true},
-    "servers:update" => {true, true},
+    # "servers:update" => {true, true},
     "servers:delete" => {true, false},
     "servers:suspend" => {true, false},
     "servers:unsuspend" => {true, false},
