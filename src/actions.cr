3e074dd63b9b35791e0264f09d780dfc3aadd768
devnote-dev <inferno0745@gmail.com>
2022-09-18 14:46:04 +0100

    refactor(actions): reimpl action as class

--- src/actions.cr
+++ src/actions.cr
@@ -1,16 +1,49 @@
-module Aerotitan::Actions
-  # key => {can_use_query, requires_data}
-  APP_ACTIONS = {
-    "users:select" => {true, false},
-    # "users:create" => {false, true},
-    # "users:update" => {true, true},
-    "users:delete" => {true, false},
-    "servers:select" => {true, false},
-    # "servers:create" => {true, true},
-    # "servers:update" => {true, true},
-    "servers:delete" => {true, false},
-    "servers:suspend" => {true, false},
-    "servers:unsuspend" => {true, false},
-    "servers:reinstall" => {true, false}
-  }
+module Aerotitan
+  class Action
+    include YAML::Serializable
+
+    KEYS = {
+      # key => {can_use_query, requires_data}
+      "users:select" => {true, false},
+      # "users:create" => {false, true},
+      # "users:update" => {true, true},
+      "users:delete" => {true, false},
+      "servers:select" => {true, false},
+      # "servers:create" => {true, true},
+      # "servers:update" => {true, true},
+      "servers:delete" => {true, false},
+      "servers:suspend" => {true, false},
+      "servers:unsuspend" => {true, false},
+      "servers:reinstall" => {true, false}
+    }
+
+    @name : String
+    @priority : Array(String) = [] of String
+    @query : Array(String) = [] of String
+    @ignore : Array(String) = [] of String
+
+    def name
+      @name
+    end
+
+    def priority
+      @priority
+    end
+
+    def number_priority
+      @priority.map(&.to_i?).uniq
+    end
+
+    def query
+      @query
+    end
+
+    def ignore
+      @ignore
+    end
+
+    def number_ignore
+      @ignore.map(&.to_i?).uniq
+    end
+  end
 end
