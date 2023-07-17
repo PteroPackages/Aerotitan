24523798864c7b99628c511ea112f2ae24997ebc
devnote-dev <devnote.dev75@gmail.com>
2023-07-17 17:48:06 +0100

    feat(query): implement initial parsing

---
+++ src/query/node.cr
@@ -0,0 +1,74 @@
+module Aero::Query
+  abstract class Node
+    getter start : Int32
+    getter stop : Int32
+
+    def initialize(@start, @stop)
+    end
+  end
+
+  abstract class Expression < Node
+  end
+
+  class Identifier < Expression
+    getter value : String
+
+    def initialize(token : Token)
+      super token.start, token.stop
+      @value = token.value
+    end
+  end
+
+  class NumberLiteral < Expression
+    getter value : Float64
+
+    def initialize(token : Token)
+      super token.start, token.stop
+      @value = token.value.to_f
+    end
+  end
+
+  class StringLiteral < Expression
+    getter value : String
+
+    def initialize(token : Token)
+      super token.start, token.stop
+      @value = token.value
+    end
+  end
+
+  class BooleanLiteral < Expression
+    getter value : Bool
+
+    def initialize(token : Token)
+      super token.start, token.stop
+      @value = token.kind.true?
+    end
+  end
+
+  class NullLiteral < Expression
+  end
+
+  class Operator < Expression
+    enum Kind
+      Equal
+      NotEqual
+      Add
+    end
+
+    getter left : Expression
+    getter op : Kind
+    getter right : Expression
+
+    def initialize(kind : Token::Kind, @left, @right)
+      super left.start, right.stop
+
+      @op = case kind
+      when .equal? then Kind::Equal
+      when .not_equal? then Kind::NotEqual
+      when .plus? then Kind::Add
+      else raise "unreachable"
+      end
+    end
+  end
+end
---
+++ src/query/parser.cr
@@ -0,0 +1,92 @@
+module Aero::Query
+  class Parser
+    enum Precedence
+      Lowest
+      Equals
+      # LessGreater
+      # Sum
+      # Product
+      # Product
+
+      def self.from(kind : Token::Kind)
+        case kind
+        when .equal?, .not_equal? then Precedence::Equals
+        else Precedence::Lowest
+        end
+      end
+    end
+
+    @tokens : Array(Token)
+    @pos : Int32
+
+    def initialize(@tokens)
+      @pos = 0
+    end
+
+    def parse : Array(Expression)
+      expressions = [] of Expression
+
+      loop do
+        expr = parse_expression :lowest
+        break if expr.nil?
+        expressions << expr
+      end
+
+      expressions
+    end
+
+    private def parse_expression(prec : Precedence) : Expression?
+      left = parse_expression current_token
+      return nil if left.nil?
+
+      loop do
+        break if current_token.kind.eol?
+        break if prec >= Precedence.from peek_token.kind
+
+        case peek_token.kind
+        when .equal?, .not_equal?
+          left = parse_operator left
+          next_token
+        else
+          break
+        end
+      end
+
+      left
+    end
+
+    private def parse_expression(token : Token) : Expression?
+      case token.kind
+      when .ident? then Identifier.new token
+      when .number? then NumberLiteral.new token
+      when .string? then StringLiteral.new token
+      when .true?, .false? then BooleanLiteral.new token
+      when .null? then NullLiteral.new token.start, token.stop
+      end
+    end
+
+    private def parse_operator(left : Node) : Expression
+      next_token
+      kind = current_token.kind
+
+      next_token
+      prec = Precedence.from current_token.kind
+      right = parse_expression prec
+      raise "Missing right-side expression for operator" if right.nil?
+
+      Operator.new kind, left, right
+    end
+
+    private def current_token : Token
+      @tokens[@pos]
+    end
+
+    private def peek_token : Token
+      @tokens[@pos + 1]
+    end
+
+    private def next_token : Token
+      @tokens[@pos += 1]
+    end
+  end
+end
